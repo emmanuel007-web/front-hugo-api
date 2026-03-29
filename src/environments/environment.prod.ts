84b0aab04a115cd49db8bfb208ec0d9e7a2c0de0
emmanuel007-web <emmanueltapasco07@gmail.com>
2026-03-29 01:24:23 -0500

    url conectada con el backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Entorno de producción — apunta al backend en Render
 export const environment = {
   production: true,
-  apiUrl: 'https://nytimes-books-backend.onrender.com/api'
+  apiUrl: 'https://api-hugo-backend.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // Entorno de desarrollo — se sustituye en producción
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api'
+  apiUrl: 'https://api-hugo-backend.onrender.com/api'
 };
